5ea04d67f3e3c41c4e74f650b3ef39de31d6028e
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2024-03-12 22:07:04 +0300

    test 1

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient<Database>(
+const client = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 );
+const supabase = () => client;
+
+export default supabase;
