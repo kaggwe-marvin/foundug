098fa1ff0c55876ca9693fd7ae83432648a795e8
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2024-03-12 03:38:21 +0300

    pay sprint

--- app/(App)/Confirmation/page.tsx
+++ app/(App)/Confirmation/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Header from "@/app/ui/Appdashboard/header";
 import Footer from "@/app/ui/landing/footer";
 import React from "react";
@@ -8,17 +7,30 @@ const Page = () => {
   const searchParams = useSearchParams();
   const getResponse = searchParams.get("resp");
   const paymentResponse = getResponse ? JSON.parse(getResponse) : null;
-  console.log(paymentResponse);
+
+  // Null check to prevent errors
+  const status = paymentResponse ? paymentResponse.status : null;
+  const message = paymentResponse ? paymentResponse.message : null;
+  const dataId =
+    paymentResponse && paymentResponse.data ? paymentResponse.data.id : null;
+  const txRef =
+    paymentResponse && paymentResponse.data ? paymentResponse.data.txRef : null;
+  const amount =
+    paymentResponse && paymentResponse.data
+      ? paymentResponse.data.amount
+      : null;
+
   return (
     <>
       <Header />
       <main className="">
         <h1>Thank you For your Payment</h1>
-        <p>status: {paymentResponse.status}</p>
-        <p>message: {paymentResponse.message}</p>
-        <p>data id:{paymentResponse.data.id}</p>
-        <p>data:{paymentResponse.data.txRef}</p>
-        <p>data amount:{paymentResponse.data.amount}</p>
+        {/* Rendering data only if available */}
+        <p>status: {status}</p>
+        <p>message: {message}</p>
+        <p>data id: {dataId}</p>
+        <p>data txRef: {txRef}</p>
+        <p>data amount: {amount}</p>
       </main>
       <Footer />
     </>
